c13002f1f92df0c53f96a38f4a22c5d90eed1566
Leo Qian <a2573705387@gmail.com>
2023-01-14 22:00:37 -0500

    change title of homepage.html

--- website/views.py
+++ website/views.py
@@ -7,7 +7,6 @@ views = Blueprint('views', __name__)
 
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
 def home():
     if request.method == "POST":
         note = request.form.get('item')
