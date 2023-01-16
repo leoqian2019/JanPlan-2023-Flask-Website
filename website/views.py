a09e492342a4fb36edcc27fdccc5ae95a027f0de
Leo Qian <a2573705387@gmail.com>
2023-01-16 17:11:04 -0500

    add code for button to mutate the list of books

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from . import db
 from .models import Book
 from flask_login import login_required, current_user
+import re
 
 views = Blueprint('views', __name__)
 
@@ -27,32 +28,36 @@ def setting():
 @login_required
 def textbook():
     if request.method == 'POST':
-        # add new textbook the user has
-        if "textbook-user-has" in request.form:
-            isbn = request.form.get('isbn')
+        isbn = request.form.get('isbn')
+        if isbn and re.match(r'^([0-9]{10}|[0-9]{13})$', isbn):
             book = Book.query.filter_by(isbn=isbn, user_id=current_user.id).first()
-            if not book:
-                new_isbn = Book(isbn=isbn, user_id=current_user.id, owning_status=1, receiving_status=0)
+            # add new textbook the user has
+            if "textbook-user-has" in request.form:
+                if not book:
+                    new_isbn = Book(isbn=isbn, user_id=current_user.id, owning_status=1, receiving_status=0)
 
-                db.session.add(new_isbn)
-            # if the book already exist, change it to pending pairing state
+                    db.session.add(new_isbn)
+                # if the book already exist, change it to pending pairing state
+                else:
+                    book.owning_status = 1
+                    book.receiving_status = 0
+            # add new textbook the user want
             else:
-                book.owning_status = 1
-                book.receiving_status = 0
+                if not book:
+                    new_isbn = Book(isbn=isbn, user_id=current_user.id, owning_status=0, receiving_status=0)
+
+                    db.session.add(new_isbn)
+                # if the book already exist, change it to pending pairing state
+                else:
+                    book.owning_status = 0
+                    book.receiving_status = 0
             db.session.commit()
-        # add new textbook the user want
+        elif "mark-receive" in request.form:
+            print(request.form.getlist('textbook-own'))
+        elif "delete" in request.form:
+            print(request.form.getlist('textbook-own'))
         else:
-            isbn = request.form.get('isbn')
-            book = Book.query.filter_by(isbn=isbn, user_id=current_user.id).first()
-            if not book:
-                new_isbn = Book(isbn=isbn, user_id=current_user.id, owning_status=0, receiving_status=0)
-
-                db.session.add(new_isbn)
-            # if the book already exist, change it to pending pairing state
-            else:
-                book.owning_status = 0
-                book.receiving_status = 0
-            db.session.commit()
+            return "The isbn you entered is not correct, please try again"
     return render_template("textbook.html", user=current_user, active_page="textbook", textbook_page="pair")
 
 
